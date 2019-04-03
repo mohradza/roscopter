a9b98468afd472d97a525c087cc259464124a071
ohrad <erush91@gmail.com>
2019-04-03 01:49:00 -0600

    added automatic take-off code, uses VINS for z-direciton pose, insert a waypoint list, linearly increasing in height, error calc based off height only.

--- roscopter/src/waypoint_manager/btraj_wp_manager.py
+++ roscopter/src/waypoint_manager/btraj_wp_manager.py
@@ -1,22 +1,36 @@
 #!/usr/bin/env python
 
 import numpy as np
-import rospy
+import rospy, tf
 from geometry_msgs.msg import PoseStamped
 from rosflight_msgs.msg import Command
 from rosflight_msgs.msg import BtrajCommand
 from rosflight_msgs.msg import RCRaw
 from quadrotor_msgs.msg import PositionCommand
 from nav_msgs.msg import Path
+from nav_msgs.msg import Odometry
 
 class hl_cmd_handler(object):
     def __init__(self):
+
+
+        # get Waypoints for Automatic Take Off
+        try:
+            self.waypoint_list = rospy.get_param('/waypoint_manager/waypoints')
+        except KeyError:
+            rospy.logfatal('waypoints not set')
+
+        self.threshold = rospy.get_param('~threshold', 0.1)
+
         # Vars
         self.rc_msg = RCRaw()
+        self.odom = Odometry()
         self.loop_rate = rospy.Rate(50)
         self.cmd_pub = rospy.Publisher('high_level_command', Command, queue_size=10)
         self.btraj_cmd_pub = rospy.Publisher('btraj_command', BtrajCommand, queue_size=10)
         rospy.Subscriber('rc_raw', RCRaw, self.rc_cb)
+        self.odom_sub = rospy.Subscriber('vins_odom', Odometry, self.odometryCallback, queue_size=5)
+
 
         self.switch = False
 
@@ -79,26 +93,76 @@ class hl_cmd_handler(object):
         command_msg1 = BtrajCommand()
         command_msg2 = BtrajCommand()
         command_msg3 = BtrajCommand()
-        while not rospy.is_shutdown():
 
-            if(self.rc_msg.values[6] < 1250):
-                rospy.loginfo_throttle(1, 'Commanding home position')
-                command_msg1 = self.home_cmd
-                command_msg1.header.stamp = rospy.Time.now()
-                self.btraj_cmd_pub.publish(command_msg1)
-            elif((self.rc_msg.values[6] > 1250) and (self.rc_msg.values[6] < 1750)):
-                rospy.loginfo_throttle(1, 'Commanding waypoints')
-                command_msg2 = self.wp_cmd2
-                command_msg2.header.stamp = rospy.Time.now()
-                self.btraj_cmd_pub.publish(command_msg2)
-            elif((self.rc_msg.values[6] < 2000) and (self.rc_msg.values[6] > 1750)):
-                rospy.loginfo_throttle(1, 'Commanding waypoints2')
-                command_msg3 = self.wp_cmd2
-                command_msg3.header.stamp = rospy.Time.now()
-                self.btraj_cmd_pub.publish(command_msg3)
+        self.take_off = 1
+        self.current_waypoint_index = 0
+
+        takeoff_msg = BtrajCommand()
+        current_waypoint = np.array(self.waypoint_list[0])
+        takeoff_msg.x = current_waypoint[0]
+        takeoff_msg.y = current_waypoint[1]
+        takeoff_msg.z = current_waypoint[2]
+        takeoff_msg.x_vel = current_waypoint[3]
+        takeoff_msg.y_vel = current_waypoint[4]
+        takeoff_msg.z_vel = current_waypoint[5]
+        current_position = np.zeros(3)
+        error = 100000000000000000
+
+        while not rospy.is_shutdown():
+            if(self.take_off == 1):
+                current_waypoint = np.array(self.waypoint_list[self.current_waypoint_index])
+                (r, p, y) = tf.transformations.euler_from_quaternion([self.odom.pose.pose.orientation.x, self.odom.pose.pose.orientation.y, self.odom.pose.pose.orientation.z, self.odom.pose.pose.orientation.w])
+                current_position = np.array([self.odom.pose.pose.position.x,
+                                             self.odom.pose.pose.position.y,
+                                             self.odom.pose.pose.position.z])
+                error = np.linalg.norm(-current_position[2] - current_waypoint[3])
+
+                if error < self.threshold:
+
+                    # Get new waypoint index
+                    self.current_waypoint_index += 1
+                    next_waypoint = np.array(self.waypoint_list[self.current_waypoint_index])
+                    takeoff_msg.x = next_waypoint[0]
+                    takeoff_msg.y = next_waypoint[1]
+                    takeoff_msg.z = next_waypoint[2]
+                    takeoff_msg.x_vel = next_waypoint[3]
+                    takeoff_msg.y_vel = next_waypoint[4]
+                    takeoff_msg.z_vel = next_waypoint[5]
+
+                    if(self.current_waypoint_index == np.shape(self.waypoint_list)[0] - 1):
+                        self.take_off = 0
+
+                rospy.loginfo_throttle(1, current_waypoint[3])
+                rospy.loginfo_throttle(1, self.odom.pose.pose.position.z)
+                rospy.loginfo_throttle(1, error)
+                rospy.loginfo_throttle(1, self.current_waypoint_index)
+                rospy.loginfo_throttle(1, self.take_off)
+
+
+            else:
+                rospy.loginfo("ahh")
+                if(self.rc_msg.values[6] < 1250):
+                    rospy.loginfo_throttle(1, 'Commanding home position')
+                    command_msg1 = self.home_cmd
+                    command_msg1.header.stamp = rospy.Time.now()
+                    self.btraj_cmd_pub.publish(command_msg1)
+                elif((self.rc_msg.values[6] > 1250) and (self.rc_msg.values[6] < 1750)):
+                    rospy.loginfo_throttle(1, 'Commanding waypoints')
+                    command_msg2 = self.wp_cmd2
+                    command_msg2.header.stamp = rospy.Time.now()
+                    self.btraj_cmd_pub.publish(command_msg2)
+                elif((self.rc_msg.values[6] < 2000) and (self.rc_msg.values[6] > 1750)):
+                    rospy.loginfo_throttle(1, 'Commanding waypoints2')
+                    command_msg3 = self.wp_cmd2
+                    command_msg3.header.stamp = rospy.Time.now()
+                    self.btraj_cmd_pub.publish(command_msg3)
                 
             self.loop_rate.sleep()
 
+    def odometryCallback(self, msg):
+        # Get error between waypoint and current state
+        self.odom = msg
+    
 if __name__ == '__main__':
     rospy.init_node('hl_cmd_handler', anonymous=True, disable_signals=True)
     hl_cmd_node = hl_cmd_handler()
