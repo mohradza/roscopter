139cc574c307cc753d22b8bbf1ee575852302ba4
Gene Rush <erush91@gmail.com>
2019-11-30 17:56:05 -0700

    first pass at high level controller for nearness control integration

---
+++ roscopter/src/waypoint_manager/A01_RaIner_HLC.py
@@ -0,0 +1,206 @@
+#!/usr/bin/env python
+
+import rospy
+from geometry_msgs.msg import PoseStamped
+from rosflight_msgs.msg import Command
+from rosflight_msgs.msg import RCRaw
+from nav_msgs.msg import Path, Odometry
+from tf.transformations import euler_from_quaternion, quaternion_from_euler
+import numpy as np
+
+class hl_cmd_handler(object):
+    def __init__(self):
+        # Vars
+        self.rc_msg = RCRaw()
+        self.loop_rate = rospy.Rate(50)
+        self.cmd_pub = rospy.Publisher('high_level_command', Command, queue_size=10)
+        rospy.Subscriber('rc_raw', RCRaw, self.rc_cb)
+        rospy.Subscriber('odom', Odometry, self.odom_cb)
+
+        self.offb_switch = False
+        self.rc_mode_chn = 6
+        self.invert_yawrate_cmd = 1
+        self.invert_yvel_cmd = 1
+        self.invert_odom_yaw_angle = 1
+
+        self.state = 'init'
+
+        self.nearness_height_agl = -1.0
+        self.nearness_xvel = 1.0
+
+        # This should be in vehicle NED frame
+        #uint8 MODE_PASS_THROUGH = 0
+        #uint8 MODE_ROLLRATE_PITCHRATE_YAWRATE_THROTTLE = 1
+        #uint8 MODE_ROLL_PITCH_YAWRATE_THROTTLE = 2
+        #uint8 MODE_ROLL_PITCH_YAWRATE_ALTITUDE = 3
+        #uint8 MODE_XPOS_YPOS_YAW_ALTITUDE = 4
+        #uint8 MODE_XVEL_YVEL_YAWRATE_ALTITUDE = 5
+        #uint8 MODE_XACC_YACC_YAWRATE_AZ = 6
+
+        #uint8 IGNORE_NONE = 0
+        #uint8 IGNORE_X = 1
+        #uint8 IGNORE_Y = 2
+        #uint8 IGNORE_Z = 4
+        #uint8 IGNORE_F = 8
+
+        self.odom_msg = Odometry()
+        self.initial_odom_state = self.odom_msg
+        self.initial_yaw = 0.0
+        self.odom_initialized = False
+        self.yaw = 0.0
+
+        self.pos_hold_cmd = Command()
+        self.pos_hold_cmd.ignore = 0
+        self.pos_hold_cmd.mode = 4
+        self.pos_hold_initialized = False
+
+        self.takeoff_initialized = False
+        self.takeoff_ramp_time_s = 3
+        self.takeoff_height_agl = self.takeoff_cmd.F
+        self.takeoff_complete = False
+
+        self.landing_F = .1
+        self.landing_complete = False
+
+
+    def rc_cb(self, data):
+        self.rc_msg = data
+        if(self.rc_msg.values[5] > 1500):
+            rospy.loginfo_throttle(5,'Offboard control is enabled.')
+            self.offb_switch = True
+        else:
+            self.offb_switch = False
+            rospy.loginfo_throttle(5,'Offboard control is disabled.')
+
+        if((self.rc_msg.values[self.rc_mode_chn] <= 1700) and (self.rc_msg.values[self.rc_mode_chn] > 1400)):
+            if(not self.takeoff_complete)
+                rospy.loginfo_once("Control Mode: Takeoff")
+                self.state == 'takeoff'
+                if(not self.takeoff_initialized):
+                    self.takeoff_start_time = rospy.Time.now()
+                    self.takeoff_initialized = True
+            else if(self.takeoff_complete and self.state == 'nearness_control')
+                self.state = 'pos_hold'
+
+        if((self.rc_msg.values[self.rc_mode_chn] > 1700) and self.takeoff_complete):
+            rospy.loginfo_once("Control Mode: Nearness control")
+            self.state == 'nearness_control'
+
+        if(self.rc_msg.values[self.rc_mode_chn] <= 1400):
+            rospy.loginfo_throttle(5, 'Control Mode: RC passthrough')
+            if(self.state == 'pos_hold'):
+                self.state == 'landing'
+
+
+    def nearness_ctrl_cb(self, data):
+        # Import nearness controller commands
+        self.nearness_yvel = data.twist.linear.y
+        self.nearness_yawrate = data.twist.angular.z
+
+    def zstate_cb(self, data):
+        self.pos_z = -data.pose.pose.position.z
+
+    def odom_cb(self, data):
+        self.odom_msg = data
+        self.pos_x = self.odom_msg.pose.pose.position.x
+        self.pos_y = -self.odom_msg.pose.pose.position.y
+
+        odom_msg_q = self.initial_odom_state.pose.pose.orientation
+        odom_msg_q_list = [odom_msg_q.x, odom_msg_q.y, odom_msg_q.z, odom_msg_q.w]
+        (roll, pitch, self.yaw) = euler_from_quaternion(odom_msg_q_list)
+        self.yaw = self.invert_odom*yaw_angle*self.yaw
+
+        if(self.state == 'init' && not self.odom_initialized):
+            self.initial_odom_state = self.odom_msg
+            odom_q = self.initial_odom_state.pose.pose.orientation
+            odom_q_list = [odom_q.x, odom_q.y, odom_q.z, odom_q.w]
+            (roll, pitch, self.initial_yaw) = euler_from_quaternion(odom_q_list)
+            self.initial_yaw = self.invert_odom_yaw_angle*self.initial_yaw
+            self.odom_initialized = True
+            rospy.loginfo_once("Initial Odometry state has been set.")
+
+    def start(self):
+        while not rospy.is_shutdown():
+            if(self.offb_switch):
+                if(self.state == 'init'):
+                    self.hl_cmd.ignore = 4
+                    self.hl_cmd.mode = 4
+                    self.hl_cmd.x = 0.0
+                    self.hl_cmd.y = 0.0
+                    self.hl_cmd.z = 0.0
+                    self.hl_cmd.F = .5
+
+                if(self.state == 'takeoff'):
+                    self.hl_cmd.ignore = 0
+                    self.hl_cmd.mode = 4
+                    self.hl_cmd.x = self.initial_odom_state.pose.pose.position.x
+                    self.hl_cmd.y = self.initial_odom_state.pose.pose.position.y
+                    self.hl_cmd.z = self.initial_yaw
+                    takeoff_time_s = (rospy.Time.now() - self.takeoff_start_time).to_sec()
+                    self.hl_cmd.F = np.clip((takeoff_time_s/self.takeoff_ramp_time_s)*self.takeoff_height_agl, 0, self.takeoff_height_agl)
+                    if(abs(self.pos_z - self.takeoff_height_agl) < self.takeoff_thresh):
+                        self.state = 'pos_hold'
+                        self.takeoff_complete = True
+
+                if(self.state == 'pos_hold'):
+                    if(not self.pos_hold_initialized):
+                        self.pos_hold_cmd.x = self.pos_x
+                        self.pos_hold_cmd.y = self.pos_y
+                        self.pos_hold_cmd.z = self.yaw
+                        self.pos_hold_cmd.F = self.pos_z
+                        self.pos_hold_initialized = True
+                    self.hl_cmd.ignore = self.pos_hold_cmd.ignore
+                    self.hl_cmd.mode = self.pos_hold_cmd.mode
+                    self.hl_cmd.x = self.pos_hold_cmd.x
+                    self.hl_cmd.y = self.pos_hold_cmd.y
+                    self.hl_cmd.z = self.pos_hold_cmd.z
+                    self.hl_cmd.F = self.pos_hold_cmd.F
+                else:
+                    self.pos_hold_initialized = False
+
+                if(self.state == 'nearness_control'):
+                    self.hl_cmd.ignore = 0
+                    self.hl_cmd.mode = 5
+                    self.hl_cmd.x = self.nearness_xvel
+                    self.hl_cmd.y = self.invert_xvel_cmd*self.nearness_yvel
+                    self.hl_cmd.z = self.nearness_yawrate
+                    self.hl_cmd.f = self.nearness_height_agl
+
+                if(self.state == 'landing'):
+                    self.hl_cmd.ignore = 0
+                    self.hl_cmd.mode = 4
+                    if(not self.landing_initialized):
+                        self.landing_cmd.x = self.pos_x
+                        self.landing_cmd.y = self.pos_y
+                        self.landing_cmd.z = self.yaw
+                        self.landing_cmd.F = self.pos_z
+                        self.landing_start_time = rospy.Time.now()
+                        self.landing_initialized = True
+
+                    self.hl_cmd.x = self.landing_cmd.x
+                    self.hl_cmd.y = self.landing_cmd.y
+                    self.hl_cmd.z = self.landing_cmd.z
+
+                    landing_time_s = (rospy.Time.now() - self.landing_start_time).to_sec()
+                    self.hl_cmd.F = np.clip(1-landing_time_s/self.landing_ramp_time_s), 0, 1)*self.landing_cmd.F
+
+                    if(self.pos_z < self.landing_thresh):
+                        self.state = 'landed'
+                        self.landing_complete = True
+                        self.takeoff_complete = False
+
+                if(self.state == 'landed'):
+                    self.hl_cmd.ignore = 4
+                    self.hl_cmd.mode = 4
+                    self.hl_cmd.x = 0.0
+                    self.hl_cmd.y = 0.0
+                    self.hl_cmd.z = 0.0
+                    self.hl_cmd.F = .5
+
+                self.cmd_pub.publish(self.hl_cmd);
+            self.loop_rate.sleep()
+
+if __name__ == '__main__':
+    rospy.init_node('hl_cmd_handler', anonymous=True)
+    hl_cmd_node = hl_cmd_handler()
+    hl_cmd_node.start()
